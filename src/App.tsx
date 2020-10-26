87bc67f3111f048c400481522625f36efb5d42af
Luke0195 <lucas201273@gmail.com>
2020-10-26 15:32:52 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+           Aplicação React + TS = 💖
         </p>
         <a
           className="App-link"
